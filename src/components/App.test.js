b58141e6d4844f926a2c7db6964cb0eaa387fb49
paakcmd <paakcmd@users.noreply.github.com>
2018-06-13 20:13:02 +0100

    Adapter Configuration

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import '../setupTests.js';
+import '../tempPolyfills.js';
+
 
 describe('App', () => {
   const app = shallow(<App />);
 
   it('renders properly', () => {
-      expect(app).toMatchSNapshop();
+      expect(app).toMatchSnapshot();
   });
 });
