01c15aa9e60857a34968cb6212ee3d45bab8a37e
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2018-06-29 10:26:40 +0700

    Remove mutation: preparing for factoring out functions

--- src/domain-logic/score.js
+++ src/domain-logic/score.js
@@ -7,24 +7,22 @@ export const scoreInterpret = (p1Score, p2Score) => {
     3: 'Forty'
   }
 
-  let name = '';
-  let name1 = '';
-  let name2 = '';
-
   const isBeforeFirstAdvantage = p1Score <= 3 && p2Score <= 3
 
   if(isBeforeFirstAdvantage) {
-    name1 = nameInterpret[p1Score]
-    name2 = nameInterpret[p2Score]
+    const name1 = nameInterpret[p1Score]
+    const name2 = nameInterpret[p2Score]
 
     if(name1 === name2){
-      name = name1 + '-ALL';
+      const name = name1 + '-ALL';
       if(name === 'Forty-ALL') {
-        name = 'Deuce';
+        return 'Deuce';
+      } else {
+        return name
       }
     }
     else {
-      name = name1 + '-' + name2;
+      return name1 + '-' + name2;
     }
 
   }
@@ -33,21 +31,20 @@ export const scoreInterpret = (p1Score, p2Score) => {
     const score_diff = Math.abs(p1Score - p2Score);
     
     if(score_diff === 1) {
-      name = 'Advantage';
+      return 'Advantage';
     } else if (score_diff >= 2) {
       if(p1Score - p2Score > 0) {
-        name = 'Player1 Won';
+        return 'Player1 Won';
       }
       else {
-        name = 'Player2 Won';
+        return 'Player2 Won';
       }
     }
     else {
-      name = 'Deuce';
+      return 'Deuce';
     }
 
   }
 
-  return name;
-
+  return '';
 }
